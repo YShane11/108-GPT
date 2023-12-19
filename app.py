b30c1ee2b167802d2b0d2c7fc1f3415b5a87efdb
YShane11 <jason20031006@gmail.com>
2023-12-19 14:52:40 +0800

    1219

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import openai
+from dotenv import dotenv_values
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
     return render_template("home.html")
 
-@app.route('/game')
-def game():
-    return render_template("game.html")
+@app.route('/execute', methods=['POST'])
+def execute():
+    config = dotenv_values("C:/Users/YShane11/env.txt")
+    openai.api_key = config["API_KEY"]
+    input_text = request.form['input']
+    print(f'執行的內容是：{input_text}')
+
+    messages = [
+        {"role": "system", "content": "你是一個AI文字美化機器人"},
+        {"role": "system", "content": "只需根據使用者輸入美化文字再輸出"},
+        {"role": "system","content": "範例輸入:我好帥 輸出:我的魅力無疑是無人能擋"},    
+        {"role": "user", "content": input_text}
+    ]
+
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=messages,
+        max_tokens=4096,
+        temperature=0.5
+    )
+
+    generated_text = response['choices'][0]['message']['content']
+    return render_template("home.html", input=input_text, output=generated_text)
 
 if __name__ == "__main__":
     app.run()
--- 後端功能/文字美化功能.py
+++ 後端功能/文字美化功能.py
@@ -9,7 +9,7 @@ openai.api_key = config["API_KEY"]
 def main(text):
     messages = [{"role": "system","content": "你是一個AI文字美化機器人"},
                 {"role": "system","content": "只需根據使用者輸入美化文字再輸出"},                
-                # {"role": "system","content": "範例輸入:我好帥 輸出:我的魅力無疑是無人能擋"},                
+                {"role": "system","content": "範例輸入:我好帥 輸出:我的魅力無疑是無人能擋"},                
                 {"role": "user", "content": text}]
 
     response = openai.ChatCompletion.create(
@@ -19,11 +19,11 @@ def main(text):
         temperature = 0.5
     )
 
-    return [i['message']['content'] for i in response['choices']]
+    return response['choices'][0]['message']['content']
 
 
 if __name__ == "__main__":
-    print(main("這歌曲真的很棒！好好聽，很多回憶在我和腦海裡出現"))
+    print(main("我討厭你"))
     # demo = gr.Interface(fn=main, inputs="text", outputs="text",title="文字美化功能",description="輸入文字",allow_flagging="never",)
 
     # demo.launch()
