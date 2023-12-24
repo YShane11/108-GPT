957d4594b9707710572390653a3ebb83b335212e
YShane11 <jason20031006@gmail.com>
2023-12-24 14:35:37 +0800

    1224

--- 後端功能/test.py
+++ 後端功能/test.py
@@ -1,8 +1,4 @@
-from pymongo import MongoClient
+from dotenv import dotenv_values
 
-
-myclient = MongoClient("mongodb+srv://YShane11:<a44993386>@school.hd1nbkk.mongodb.net/")
-Test = myclient.學系學群學類.學群
-
-for i in Test.find():
-    print(i)
+config = dotenv_values("C:/Users/jason/.env.txt")
+print(config["QQ"])
--- 後端功能/文字美化功能.py
+++ 後端功能/文字美化功能.py
@@ -1,17 +1,37 @@
 import openai
 from dotenv import dotenv_values
 import gradio as gr
-
-config = dotenv_values("C:/Users/YShane11/env.txt")
-openai.api_key = config["API_KEY"]
+from pymongo import MongoClient
 
 
-def main(text):
+
+def database():
+    myclient = MongoClient("mongodb+srv://YShane11:a44993386@school.hd1nbkk.mongodb.net/")
+    alldepartment = myclient.學系學群學類.學系
+
+    department = []
+    for i in alldepartment.find():
+        department.append(i)
+    return department
+
+def main(department, text):
+    config = dotenv_values("C:/Users/jason/.env.txt")
+    openai.api_key = config["API_KEY"]  
+
+    allschooldata= database()
+    for i in allschooldata:
+        if i['學校']+i['系所'] == department:
+            aimdepartment = i
+            break
+
     messages = [{"role": "system","content": "你是一個AI文字美化機器人"},
                 {"role": "system","content": "只需根據使用者輸入美化文字再輸出"},                
-                {"role": "system","content": "範例輸入:我好帥 輸出:我的魅力無疑是無人能擋"},                
+                {"role": "system","content": "範例輸入:我好帥 輸出:我的魅力無疑是無人能擋"},     
+                {"role": "system","content": f"{department}所適合的性格特質:{aimdepartment['性格特質']}"},
+                {"role": "system","content": f"學生的性格特質:很有自信"},
+                {"role": "user", "content": '如提供學生的性格特質與校系適合的性格特質有雷同，可就此特質適當地多加描述'},        
                 {"role": "user", "content": text}]
-
+    
     response = openai.ChatCompletion.create(
         model = "gpt-4",
         messages = messages,
@@ -23,7 +43,7 @@ def main(text):
 
 
 if __name__ == "__main__":
-    print(main("我討厭你"))
+    print(main("國立臺灣大學中國文學系","我討厭你"))
     # demo = gr.Interface(fn=main, inputs="text", outputs="text",title="文字美化功能",description="輸入文字",allow_flagging="never",)
 
     # demo.launch()
