cbde385a8c8e70dc5cfcf3964ada32f0f18ddda8
YShane11 <jason20031006@gmail.com>
2024-03-16 16:50:15 +0800

    0316

--- 後端功能/文字美化功能.py
+++ 後端功能/文字美化功能.py
@@ -25,20 +25,26 @@ def main(department, text):
             break
 
     messages = [{"role": "system","content": "你是一個AI文字美化機器人,只需根據使用者輸入美化文字再輸出"},                                
-                {"role": "user","content": f"{department}所適合的性格特質:{aimdepartment['性格特質']}"},
-                {"role": "user", "content": f'如內容與「{department}適合的性格特質」有雷同的特質，可就此特質適當地多加描述'},        
-                {"role": "user", "content": '不要過度誇示'},        
+                {"role": "system","content": f"{department}所適合的性格特質:{aimdepartment['性格特質']}"},
+                {"role": "system", "content": f'如內容與「{department}適合的性格特質」有雷同的特質，可就此特質適當地多加描述'},        
+                {"role": "user", "content": '注意:不要過度誇示'},   
+                {"role": "user", "content": '語言:zh-Tw'},
+                {"role": "user", "content": '輸出:{ 修改後的輸出 }'},
                 {"role": "user", "content": text}]
 
     response = openai.ChatCompletion.create(
         model = "gpt-4-0125-preview",
         messages = messages,
         max_tokens = 4096,
+        n=5,
         temperature = 0.9
     )
 
     return response['choices'][0]['message']['content'] 
 
-
 if __name__ == "__main__":
-    print(main("國立臺灣大學中國文學系","我很聰明"))
+    print(main("國立臺灣大學中國文學系",'''
+ 我覺得我滿喜歡跟別人打交道，也滿想了解他們的。不過，我不只是單單想了解，我也挺在乎怎麼應用這些知識的。所以，我對消費心理學之類的課程超有興趣。我聽說輔仁大學的心理學系挺出名的，對我這樣的高中生來說，蠻吸引人的。我想要在這方面專業技能和知識上有所提升，相信這所大學會有不錯的教授和課程能夠培養我。我喜歡了解別人，陪伴他們，關心他們，也許因為我自己也是需要被關心的。所以，我會盡力去理解別人，有耐心學習這種科學又人文的科系。
+               '''))
+    
+
